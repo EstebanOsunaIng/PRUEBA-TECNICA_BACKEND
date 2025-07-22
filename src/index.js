65fab2ba367448ff799f7b8f1db142e2828860af
Esteban Osuna <estebanosunaing@gmail.com>
2025-07-21 19:05:32 -0500

    Lanzamos un servidor usando express en el puerto 3000

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-console.log("Hola soy un sencillo Ecommerce");
+const express = require ( 'express');
+const app = express();
+
+//* Lanzar el servidor usando express" */ 
+
+app.listen (3000, function(){
+    console.log('SERVIDOR EN http://localhost:3000');
+});
 
